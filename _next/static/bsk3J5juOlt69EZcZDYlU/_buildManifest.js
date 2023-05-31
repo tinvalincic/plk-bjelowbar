a62be82818324c8bce6c532d72666b5d98f25edc
tinvalincic <tinvalinc@gmail.com>
2023-05-31 16:26:51 +0200

    build web

--- _next/static/bsk3J5juOlt69EZcZDYlU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/48cb89de35a9e1fd.css","static/chunks/pages/index-3f621f5ad1fcccd5.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
